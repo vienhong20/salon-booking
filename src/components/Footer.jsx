18b57a75fe5303cbca9df1c341f8e72e94f8f29a
VIEN HONG <vienhong88@gmail.com>
2023-04-05 16:33:22 -0400

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-import { BsFacebook, BsTwitter, BsYoutube, BsBehance } from'react-icons/bs'
+import { BsFacebook, BsTwitter, BsYoutube, BsBehance } from 'react-icons/bs'
 
 function Footer() {
   return (
@@ -21,7 +21,7 @@ function Footer() {
 const Foot = styled.footer`
 background-color: var(--primary-color);
 color: #fff;
-display: flex;
+display:flex;
 justify-content: space-between;
 align-items: center;
 padding: 5rem 10rem;
@@ -34,7 +34,7 @@ padding: 5rem 10rem;
     transition: 0.5s ease-in-out;
     &:hover {
       color: var(--secondary-color);
-    }
+    }   
   }
 }
 `;
--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styled from "styled-components";
+import { FaChevronUp } from "react-icons/fa";
 
 function ScrollToTop() {
+  const [visible, setVisible] =useState(false);
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 100 ? setVisible(true) : setVisible(false);
+  })
   return (
-    <div>ScrollToTop</div>
+    <Div>
+      <a href="#" className={`${visible ? "block" : "none"}`}>
+        <FaChevronUp />
+      </a>
+    </Div>
   )
 }
+
+const Div = styled.div`
+
+`;
 
 export default ScrollToTop
