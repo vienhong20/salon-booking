02666fb31ccb110b6133781fde6f5bf758b5a220
VIEN HONG <vienhong88@gmail.com>
2023-04-05 15:21:45 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import { BsFacebook, BsTwitter, BsYoutube, BsBehance } from'react-icons/bs'
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Foot>
+      <span>
+        &copy React Website Transition Animation
+      </span>
+      <div>
+        <BsFacebook />
+        <BsTwitter />
+        <BsYoutube />
+        <BsBehance />
+      </div>
+    </Foot>
+  );
 }
+
+const Foot = styled.footer`
+background-color: var(--primary-color);
+color: #fff;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 5rem 10rem;
+.footer__social__icons {
+  display: flex;
+  gap: 2rem;
+  svg {
+    font-size: 1.4rem;
+    cursor: pointer;
+    transition: 0.5s ease-in-out;
+    &:hover {
+      color: var(--secondary-color);
+    }
+  }
+}
+`;
 
 export default Footer
