bfbab89edce6410679ec7c836ca970a6916c64d5
VIEN HONG <vienhong88@gmail.com>
2023-04-07 15:57:22 -0400

    Update

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -17,7 +17,28 @@ function ScrollToTop() {
 }
 
 const Div = styled.div`
-
+max-width: 100vw;
+.none {
+  opacity: 0;
+  visibility: hidden;
+}
+a {
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  background-color: var(--secondary-color);
+  padding: 1rem;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  transition: 0.4s ease-in-out;
+  svg {
+    color: #fff;
+    font-size: 1.3rem
+  }
+}
 `;
 
 export default ScrollToTop
