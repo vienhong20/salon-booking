b94fae4be62611c9e37b1c18d1f257d9414ef5ff
VIEN HONG <vienhong88@gmail.com>
2023-04-08 16:30:20 -0400

    Update animation.js

---
+++ src/animation.js
@@ -0,0 +1,8 @@
+export const homeAnimation = {
+    hidden: { x: -400, opacity: 0 },
+    show: { x: 0, opacity: 1 },
+};
+export const homeInfoAnimation = {
+    hidden: { x: 100, opacity: 0 },
+    show: { x:0, opacity: 1 },
+};
